770e20de8e7cb2515a0c5fb6b27fc4a9434121e7
Suresh Boddu <sureshkumarboddu9.8@gmail.com>
2025-04-20 12:19:14 -0500

    Added property in Application User

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -12,6 +12,8 @@ namespace BidUser.Models
 
         public string? FirstName { get; set; }
 
+        public string? MiddleName { get; set; }
+
         public string? LastName { get; set; }
 
         //public string? Email { get; set; }
