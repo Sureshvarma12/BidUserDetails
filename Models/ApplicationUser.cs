607be16044884fbd093dcf50beb71b9d110bef86
Suresh Boddu <sureshkumarboddu9.8@gmail.com>
2025-04-20 14:04:13 -0500

    I have identidtity poroperties

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -12,8 +12,6 @@ namespace BidUser.Models
 
         public string? FirstName { get; set; }
 
-        public string? MiddleName { get; set; }
-
         public string? LastName { get; set; }
 
         //public string? Email { get; set; }
